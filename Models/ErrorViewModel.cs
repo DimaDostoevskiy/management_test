47b6880142673747dcbe276042e81fb46ad4bf35
DimaDostoevskiy <dostoevskiy@list.ru>
2022-03-11 18:41:46 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace asu_management.mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
